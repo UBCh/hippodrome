fdd1fc4fb7ac19a01cbd8a27dcbec4fd6a74b945
UBCh <u2b127@gmail.com>
2023-01-08 18:32:25 +0300

    add mockito add junit add log4j add HorseTest

---
+++ src/main/java/HippodromeTest.java
@@ -0,0 +1,25 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.runners.Parameterized;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+public class HippodromeTest {
+
+    @Test
+    public void getHorses() {
+    }
+
+    @Test
+    public void move() {
+    }
+
+    @Test
+    public void getWinner() {
+    }
+}
